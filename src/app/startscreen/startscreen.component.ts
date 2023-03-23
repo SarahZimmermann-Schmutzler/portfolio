ec1954be874f5b46258bb548823d88d8576c5d14
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-03-23 09:20:32 +0100

    links on page

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./startscreen.component.scss']
 })
 export class StartscreenComponent {
-
+  toContact() {
+    window.location.href='#contact';
+  }
 }
