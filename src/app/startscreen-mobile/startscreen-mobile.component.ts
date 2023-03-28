8742b832a78291ea36c0aef7242289518d369466
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-03-28 10:57:57 +0200

    startscreen mobile ready

--- src/app/startscreen-mobile/startscreen-mobile.component.ts
+++ src/app/startscreen-mobile/startscreen-mobile.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./startscreen-mobile.component.scss']
 })
 export class StartscreenMobileComponent {
-
+  toContact() {
+    window.location.href='#contact';
+  }
 }
