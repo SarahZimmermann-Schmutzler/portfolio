68bcedcfa089c8a57790a9025c07de4ca4ab8278
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-03-29 11:43:47 +0200

    projects mobile

--- src/app/projects-mobile/projects-mobile.component.ts
+++ src/app/projects-mobile/projects-mobile.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects-mobile.component.scss']
 })
 export class ProjectsMobileComponent {
+  gitRof() {
+    window.open('https://github.com/SarahZimmermann-Schmutzler/Ring-of-Fire', '_blank');
+  }
 
+  gitPollo() {
+    window.open('https://github.com/SarahZimmermann-Schmutzler/El_Pollo_Loco', '_blank');
+  }
+
+  gitJoin() {
+    window.open('https://github.com/SarahZimmermann-Schmutzler/join_new', '_blank');
+  }
+
+  serverRof() {
+    window.open('https://ring-of-fire-54fa3.web.app', '_blank');
+  }
+
+  serverPollo() {
+    window.open('https://sarah-zimmermann-schmutzler.developerakademie.net/elPolloLoco/index.html', '_blank');
+  }
+
+  serverJoin() {
+    window.open('https://sarah-zimmermann-schmutzler.developerakademie.net/join_new/index.html', '_blank');
+  }
 }
