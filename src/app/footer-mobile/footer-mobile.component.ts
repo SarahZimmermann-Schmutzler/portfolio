beb83b848709449b3c87d21bfdbbc78668cbd4c2
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-03-30 11:16:17 +0200

    header mobile ready

--- src/app/footer-mobile/footer-mobile.component.ts
+++ src/app/footer-mobile/footer-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer-mobile',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer-mobile.component.scss']
 })
 export class FooterMobileComponent {
-
+  constructor(public translation: TranslateService) {
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    // translate.use('en');
+    // fügen das in HTML als Link ein, um Sprachen zu switchen
+  }
 }
--- src/app/header-mobile/header-mobile.component.ts
+++ src/app/header-mobile/header-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header-mobile',
@@ -8,11 +9,37 @@ import { Component, Input } from '@angular/core';
 export class HeaderMobileComponent {
   @Input() openMenu = false;
 
+  constructor(public translation: TranslateService) {
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    // translate.use('en');
+    // fügen das in HTML als Link ein, um Sprachen zu switchen
+  }
+
   showMenu() {
     this.openMenu = true;
   }
 
   closeMenu() {
     this.openMenu = false;
+  }
+
+  showAbout() {
+    this.openMenu = false;
+    window.location.href = '#about-mobile';
+  }
+
+  showSkills() {
+    this.openMenu = false;
+    window.location.href = '#skills-mobile';
+  }
+
+  showProjects() {
+    this.openMenu = false;
+    window.location.href = '#projects-mobile';
+  }
+
+  showContact() {
+    this.openMenu = false;
+    window.location.href = '#contact-mobile';
   }
 }
