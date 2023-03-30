d7bfad42530e1482a46503ce4b7ea02b4e325790
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-03-30 15:11:46 +0200

    render projects beginning

--- src/app/projects-mobile/projects-mobile.component.ts
+++ src/app/projects-mobile/projects-mobile.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-mobile',
   templateUrl: './projects-mobile.component.html',
   styleUrls: ['./projects-mobile.component.scss']
 })
-export class ProjectsMobileComponent {
+export class ProjectsMobileComponent implements OnInit {
+  projects = [
+    {
+      'image': 'ring.png',
+      'title': 'RING OF FIRE',
+      'langs': 'Angular | Firebase | TypeScript | HTML | SCSS',
+      'description': 'Popular Card Game "Ring of Fire" as a multiplayer App. Coded with Angular and Firebase.',
+      'tryBtn': 'serverRof()',
+      'gitBtn': 'gitRof()'
+    },
+
+    {
+      'image': 'pollo.png',
+      'title': 'EL POLLO LOCO',
+      'langs': 'JavaScript | HTML | CSS',
+      'description': 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      'tryBtn': 'serverPollo()',
+      'gitBtn': 'gitPollo()'
+    },
+
+    {
+      'image': 'join.png',
+      'title': 'JOIN',
+      'langs': 'JavaScript | HTML | CSS',
+      'description': 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      'tryBtn': 'serverJoin()',
+      'gitBtn': 'gitJoin()'
+    }
+  ];
+
+  ngOnInit() {
+    
+  }
+
   gitRof() {
     window.open('https://github.com/SarahZimmermann-Schmutzler/Ring-of-Fire', '_blank');
   }
