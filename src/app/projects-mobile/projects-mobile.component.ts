5fe217bff60707010b9c0d97564993b47d246af4
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-03-31 10:39:24 +0200

    function fpr btns

--- src/app/projects-mobile/projects-mobile.component.ts
+++ src/app/projects-mobile/projects-mobile.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects-mobile',
   templateUrl: './projects-mobile.component.html',
   styleUrls: ['./projects-mobile.component.scss']
 })
-export class ProjectsMobileComponent implements OnInit {
+export class ProjectsMobileComponent {
+
+  constructor(private router: Router) {}
+
   projects = [
     {
       'image': 'ring.png',
       'title': 'RING OF FIRE',
       'langs': 'Angular | Firebase | TypeScript | HTML | SCSS',
       'description': 'Popular Card Game "Ring of Fire" as a multiplayer App. Coded with Angular and Firebase.',
-      'tryBtn': 'serverRof()',
-      'gitBtn': 'gitRof()'
+      'tryBtn': 'serverRof',
+      'gitBtn': 'gitRof'
     },
 
     {
@@ -21,8 +25,8 @@ export class ProjectsMobileComponent implements OnInit {
       'title': 'EL POLLO LOCO',
       'langs': 'JavaScript | HTML | CSS',
       'description': 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
-      'tryBtn': 'serverPollo()',
-      'gitBtn': 'gitPollo()'
+      'tryBtn': 'serverPollo',
+      'gitBtn': 'gitPollo'
     },
 
     {
@@ -30,13 +34,28 @@ export class ProjectsMobileComponent implements OnInit {
       'title': 'JOIN',
       'langs': 'JavaScript | HTML | CSS',
       'description': 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      'tryBtn': 'serverJoin()',
-      'gitBtn': 'gitJoin()'
+      'tryBtn': 'serverJoin',
+      'gitBtn': 'gitJoin'
     }
   ];
 
-  ngOnInit() {
+
+  projectServer() {
+    if('serverRof') {
+      window.open('https://ring-of-fire-54fa3.web.app', '_blank');
+    }
     
+    if('serverPollo') {
+      window.open('https://sarah-zimmermann-schmutzler.developerakademie.net/elPolloLoco/index.html', '_blank');
+    }
+
+    if('serverJoin') {
+      window.open('https://sarah-zimmermann-schmutzler.developerakademie.net/join_new/index.html', '_blank');
+    }
+  }
+
+  projectGit() {
+
   }
 
   gitRof() {
