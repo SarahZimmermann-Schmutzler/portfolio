ab7d32d0cad8b74021b4a590cf3c188ebf59d7ae
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-06-07 15:07:30 +0200

    title for skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,34 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-  titleOne = false;
+  // titleOne = false;
+
+  hoveredIndex: number | null = null;
 
   firstRow = [
     {
       source: 'assets/img/skills/angular.png',
-      name: 'angular'
-    },
-
-    {
-      source: 'assets/img/skills/ts.png',
-      name: 'typesript'
+      title: 'angular'
     },
 
     {
       source: 'assets/img/skills/js.png',
-      name: 'javascript'
+      title: 'javascript'
+    },
+
+    {
+      source: 'assets/img/skills/css.png',
+      title: 'css'
     },
 
     {
       source: 'assets/img/skills/html.png',
-      name: 'html'
+      title: 'html'
+    },
+
+    {
+      source: 'assets/img/skills/chatgpt.png',
+      title: 'chatgpt'
+    },
+
+    {
+      source: 'assets/img/skills/git.png',
+      title: 'git'
     },
 
     {
       source: 'assets/img/skills/scrum.png',
-      name: 'scrum'
+      title: 'scrum'
+    },
+
+    {
+      source: 'assets/img/skills/md.png',
+      title: 'Material Design'
     },
 
   ]
+
+  showTitle(index: number) {
+    this.hoveredIndex = index;
+  }
+
+  showImg() {
+    this.hoveredIndex = null;
+  }
 
 }
