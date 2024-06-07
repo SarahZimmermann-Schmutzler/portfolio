47f5ae65a34e26f805cba176a547aade5e9087ff
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-06-07 13:07:23 +0200

    new style for skill icons

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  titleOne = false;
+
+  firstRow = [
+    {
+      source: 'assets/img/skills/angular.png',
+      name: 'angular'
+    },
+
+    {
+      source: 'assets/img/skills/ts.png',
+      name: 'typesript'
+    },
+
+    {
+      source: 'assets/img/skills/js.png',
+      name: 'javascript'
+    },
+
+    {
+      source: 'assets/img/skills/html.png',
+      name: 'html'
+    },
+
+    {
+      source: 'assets/img/skills/scrum.png',
+      name: 'scrum'
+    },
+
+  ]
 
 }
